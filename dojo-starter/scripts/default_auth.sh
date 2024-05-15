afe0c9057a290137be64fd4156be38cd179f20fa
0xlonescv <0xbuilderham@gmail.com>
2024-05-15 22:48:07 +0800

    ➕ Add scarb deps and auth script

--- dojo-starter/scripts/default_auth.sh
+++ dojo-starter/scripts/default_auth.sh
@@ -12,8 +12,7 @@ echo "--------------------------------------------------------------------------
 
 # enable system -> models authorizations
 sozo auth grant --world $WORLD_ADDRESS --wait writer \
-  Position,dojo_starter::systems::actions::actions\
-  Moves,dojo_starter::systems::actions::actions\
+  Player,dojo_starter::systems::lobby::lobby\
   >/dev/null
 
 echo "Default authorizations have been successfully set."
